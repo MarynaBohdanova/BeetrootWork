83264746e0273829f80c47e37c0bf435af793d6a
github-entity <bogdanushka@gmail.com>
2022-09-04 17:31:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BeetrootWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
