cd10f564506dfcbabdb3332d7bab812d3562a805
github-entity <bogdanushka@gmail.com>
2022-09-04 18:30:10 +0300

    create a conflict - start

--- Program.cs
+++ Program.cs
@@ -9,8 +9,11 @@ namespace BeetrootWork
             string message = "Step 1 : Create your own repository, commit any C# program to this repository";
             Console.WriteLine(message);
 
-            string message2 = "make any change and commit this change.";
+            string message2 = "Make any change and commit this change.";
             Console.WriteLine(message2);
+
+            string message3 = "Resolve conflict";
+            Console.WriteLine(message3);
         }
     }
 }
